03fda3c32d25b03af5854ecb981e7ef98604251f
RodrigoJacznik <jacznik.rodrigo@gmail.com>
2016-12-18 18:08:31 -0300

    Agrega item-list

---
+++ src/client/app/components/ItemList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Item from './Item.js';
+
+const ItemList = () => {
+    return (
+        <div className="item-list">
+            <Item />
+            <Item />
+        </div>
+    );
+};
+
+export default ItemList;
