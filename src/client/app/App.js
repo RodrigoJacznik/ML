191aea1c3a9a7f5ee797d59958b5555d9cdbfd05
RodrigoJacznik <jacznik.rodrigo@gmail.com>
2016-12-18 18:10:59 -0300

    Importa todos los estilos de los componentes

--- src/client/app/App.js
+++ src/client/app/App.js
@@ -3,13 +3,19 @@ import React, { Component } from 'react';
 require('./styles/app.scss');
 
 import SearchBox from './components/SearchBox';
+import ItemList from './components/ItemList';
+import ItemDetail from './components/ItemDetail';
+import Breadcrumb from './components/Breadcrumb';
 
 export default class App extends Component {
-  render() {
-    return (
-        <div>
-            <SearchBox />
-        </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <SearchBox />
+                <Breadcrumb />
+                <ItemList />
+                <ItemDetail />
+            </div>
+        );
+    }
 }
