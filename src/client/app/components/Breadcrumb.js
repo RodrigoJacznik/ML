be47ff1119c94f60d6646a5fa4edc52ae066a589
RodrigoJacznik <jacznik.rodrigo@gmail.com>
2016-12-18 18:07:01 -0300

    Agrega breadcrumb

---
+++ src/client/app/components/Breadcrumb.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Breadcrumb = () => {
+    return (
+        <div className="breadcrumb-container">
+            <ul className="breadcrumb">
+                <li className="breadcrumb__item">Apple</li>
+                <li className="breadcrumb__item">Celular</li>
+                <li className="breadcrumb__item">Iphone</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Breadcrumb;
