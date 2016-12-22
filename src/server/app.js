c6aaaec356ef0b94b8d53723cb33ab52b0858842
RodrigoJacznik <jacznik.rodrigo@gmail.com>
2016-12-22 03:02:51 -0300

    Agrega morgan para loggear request

--- src/server/app.js
+++ src/server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const path = require('path');
+const morgan = require('morgan');
 
 const PUBLIC_PATH = path.join(__dirname, '..', 'client', 'public');
 
@@ -9,6 +10,8 @@ const IS_PRO = process.env.NODE_ENV === 'production';
 const PORT = IS_PRO ? process.env.PORT : 8080;
 
 const app = express();
+
+app.use(morgan('combined'));
 
 app.use(session({
     secret: 'Esto es un secreto',
